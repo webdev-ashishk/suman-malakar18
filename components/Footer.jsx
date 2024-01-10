6d5ac95b67273c1c6342fa31336e99d12381ee0c
webdev-ashishk <webdev.ashishk.com>
2024-01-10 20:29:50 +0530

    fixed footer to bottom #### Good checkpoints

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <div className="m-2 mt-9">
+    <div className="m-2 mt-9 footer">
       <div className="text-blue-500  px-2 py-2 border-t-2 w-full text-center">
         SUMAN MALAKAR @{currentYear}
       </div>
